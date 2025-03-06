4dfd3f9c4416719905b55a2d814709007fdd54ef
Gustavo Moura <gustavo.moura@serpro.gov.br>
2025-03-06 15:48:05 -0300

    Habilitando todos os testes

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -176,7 +176,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .and('have.attr', 'target', '_blank')
   })
 
-  it.only('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+  it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
     cy.contains('a', 'Política de Privacidade')
       .invoke('removeAttr', 'target')
       .click()
